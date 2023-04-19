0e0ad43dde1aff18553ff7759005717ba157fa13
Pav85 <pawel.werbowy@gmail.com>
2023-04-19 06:41:40 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 
 const App = () => {
+  const surpriseOptions = [
+    "A kangaroo on the bicycle",
+    "Dancing bear in a pink dress",
+    "Realistic photo of Hitler playing chess with Jesus"
+  ]
  
   return (
     <div className="App">
       <section className="search-section">
-        <p>Enter detailed description of a picture</p>
+        <p>Enter detailed description of a picture 
+          <span className="surprise">Surprise me</span>
+          </p>
       </section>
       <section className="image-section"></section>
     </div>
