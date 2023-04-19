3b7143ed9479d1daf4a97700b36f0ef91752cc1d
Pav85 <pawel.werbowy@gmail.com>
2023-04-19 06:35:51 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ const App = () => {
  
   return (
     <div className="App">
-      <section></section>
-      <section></section>
+      <section className="search-section">
+        <p>Enter detailed description of a picture</p>
+      </section>
+      <section className="image-section"></section>
     </div>
   )
 }
