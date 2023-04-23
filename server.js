5668f07550ac6a10b0c708207d0a00373006fe3f
Pav85 <pawel.werbowy@gmail.com>
2023-04-23 18:35:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,20 +7,20 @@ app.use(cors());
 app.use(express.json());
 require("dotenv").config();
 
-app.post("/images", () => {
-    const { Configuration, OpenAIApi } = require("openai");
+const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-const response = await openai.createImageEdit(
-  fs.createReadStream("otter.png"),
-  fs.createReadStream("mask.png"),
-  "A cute baby sea otter wearing a beret",
-  2,
-  "1024x1024"
-);
 
+app.post("/images", async (req, res) => {
+  const response = await openai.createImageEdit(
+    fs.createReadStream("otter.png"),
+    fs.createReadStream("mask.png"),
+    "A cute baby sea otter wearing a beret",
+    2,
+    "1024x1024"
+  );
 });
 
 app.listen(PORT, () => console.log("Your server is running on PORT " + PORT));
